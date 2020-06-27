6a5033ae438185e62e465490b53da052be740824
Petr Mikheev <ptmikheev@gmail.com>
2020-06-27 14:30:46 +0200

    fix getInputManager -> getWindowManager Was broken by https://gitlab.com/ptmikheev/openmw/-/commit/f990150c497483a099adfcf44192076e31d72d37

--- buildscripts/patches/openmw/android_main.cpp
+++ buildscripts/patches/openmw/android_main.cpp
@@ -2,8 +2,7 @@ int stderr = 0; // Hack: fix linker error
 
 #include "SDL_main.h"
 #include "engine.hpp"
-#include "mwbase/inputmanager.hpp"
-#include "mwinput/inputmanagerimp.hpp"
+#include "mwbase/windowmanager.hpp"
 #include <SDL_gamecontroller.h>
 #include <SDL_mouse.h>
 #include <SDL_events.h>
@@ -90,7 +89,7 @@ extern "C" void Java_org_libsdl_app_SDLActivity_omwSurfaceDestroyed(JNIEnv *env,
     ctx = g_viewer->getCamera()->getGraphicsContext();
     ctx->add(op);
 
-    auto inp = (MWInput::InputManager *)MWBase::Environment::get().getInputManager();
+    auto inp = MWBase::Environment::get().getWindowManager();
     if (inp)
         inp->windowVisibilityChange(false);
 }
@@ -103,7 +102,7 @@ extern "C" void Java_org_libsdl_app_SDLActivity_omwSurfaceRecreated(JNIEnv *env,
     ctx = g_viewer->getCamera()->getGraphicsContext();
     ctx->add(op);
 
-    auto inp = (MWInput::InputManager *)MWBase::Environment::get().getInputManager();
+    auto inp = MWBase::Environment::get().getWindowManager();
     if (inp)
         inp->windowVisibilityChange(true);
 }
